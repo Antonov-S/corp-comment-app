bcd86191f6b490495ba903cd675e718559bb030b
Antonov-S <anto1nov1@gmail.com>
2024-01-15 16:45:05 +0200

    Making Feedback Form input Controlled and make Counter dynamic

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setNewText] = useState("");
+  const charactersLeft = MAX_CHARACTERS - text.length;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (MAX_CHARACTERS - newText.length < 0) {
+      return;
+    }
+    setNewText(newText);
+  };
+
   return (
     <form className="form">
       <textarea
         id="feedback-textarea"
         placeholder="blabla"
         spellCheck={false}
+        value={text}
+        onChange={handleChange}
       />
 
       <label htmlFor="feedback-textarea">
@@ -12,7 +28,7 @@ export default function FeedbackForm() {
       </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charactersLeft}</p>
         <button>
           <span>Submit</span>
         </button>
